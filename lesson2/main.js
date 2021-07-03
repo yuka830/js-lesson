2529d1fbb56bb640d49b0d7c2e803c9a28f67d5d
Yuka <66966699+yuka830@users.noreply.github.com>
2021-07-03 16:08:26 -0700

    fix: Implementing the img property in insertAdjacentElement replaced appendChild(img) with insertAdjacentElement("afterbegin", img). And also organized the code.

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,17 +1,12 @@
-// create list property
+const ul = document.getElementById("js-lists");
 const li = document.createElement("li");
-
-// create anchor property
 const a = document.createElement("a");
+const img = document.createElement("img");
+
 a.href = "1.html";
 a.textContent = "これです";
 
-// create img property
-const img = document.createElement("img");
 img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
-// add them in ul property
-const ul = document.getElementById("js-lists");
-
-ul.appendChild(li).appendChild(a).appendChild(img);
+ul.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
