ba80fdf302828e2cea3ffe0c560e39e1221ca40d
yuka <66966699+yuka830@users.noreply.github.com>
2021-07-07 23:54:44 -0700

    Feat lesson4
    add main.js and implemented using 'foreach'

---
+++ lesson4/main4.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const ul = document.getElementById("js-lists");
+const fragment = document.createDocumentFragment();
+const listItems = [
+  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
+
+listItems.forEach( key => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  
+  a.href = `/${key.to}`;
+  a.textContent = key.text;
+  img.src = key.img;
+  img.alt = key.alt;
+
+  fragment.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
+})
+
+ul.appendChild(fragment);
