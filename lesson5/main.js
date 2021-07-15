bdf355d74de77c1f4f523fd5d61a6a6940800f73
yuka <66966699+yuka830@users.noreply.github.com>
2021-07-14 17:29:08 -0700

    Update: deconstructed insertList function
    Returned constants of ul and fragment to global scope.

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,15 +1,16 @@
 "use strict";
 
+const ul = document.getElementById("js-lists");
+const fragment = document.createDocumentFragment();
 const fetchedData = new Promise((resolve, reject) => {
   const listItems = [
     { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
-    { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+    { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
   ];
   resolve(listItems);
 });
 
 function createNewList(data) {
-  const fragment = document.createDocumentFragment();
   data.forEach((key) => {
     const li = document.createElement("li");
     const a = document.createElement("a");
@@ -25,12 +26,7 @@ function createNewList(data) {
       .appendChild(a)
       .insertAdjacentElement("afterbegin", img);
   });
-  return fragment;
+  ul.appendChild(fragment);
 }
 
-function insertList(data) {
-  const ul = document.getElementById("js-lists");
-  ul.appendChild(data);
-}
-
-fetchedData.then((listItems) => insertList(createNewList(listItems)));
+fetchedData.then((listItems) => createNewList(listItems));
