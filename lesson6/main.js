743e8820b5e5f959f3a0a61d9df4011ccd56d38c
yuka <66966699+yuka830@users.noreply.github.com>
2021-07-18 23:14:26 -0700

    Feat lesson6
    copy file from lesson5

---
+++ lesson6/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const ul = document.getElementById("js-lists");
+const fragment = document.createDocumentFragment();
+const fetchedData = new Promise((resolve, reject) => {
+  const listItems = [
+    { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+    { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
+  ];
+  resolve(listItems);
+});
+
+function createNewList(data) {
+  data.forEach((key) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = `/${key.to}`;
+    a.textContent = key.text;
+    img.src = key.img;
+    img.alt = key.alt;
+
+    fragment
+      .appendChild(li)
+      .appendChild(a)
+      .insertAdjacentElement("afterbegin", img);
+  });
+  ul.appendChild(fragment);
+}
+
+fetchedData.then((listItems) => createNewList(listItems));
