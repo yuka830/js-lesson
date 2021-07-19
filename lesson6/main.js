f02bf4ff1ea28f07625f26f9cf2106fba843171d
yuka <66966699+yuka830@users.noreply.github.com>
2021-07-18 23:29:19 -0700

    Feat lesson6
    Imprementing setTimeout function for lesson5

--- lesson6/main.js
+++ lesson6/main.js
@@ -7,7 +7,9 @@ const fetchedData = new Promise((resolve, reject) => {
     { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
     { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
   ];
-  resolve(listItems);
+  setTimeout(() => {
+    resolve(listItems);
+  }, 3000);
 });
 
 function createNewList(data) {
