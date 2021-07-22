23f90ccb9b9230c1f795760c6f6033aa1f6b639d
yuka <66966699+yuka830@users.noreply.github.com>
2021-07-21 17:42:41 -0700

    Style: lesson7
    Implementing the elements about loading image in JS

--- lesson7/main.js
+++ lesson7/main.js
@@ -1,10 +1,16 @@
 "use strict";
 
+const wrapper = document.getElementById("js-wrapper");
 const ul = document.getElementById("js-lists");
 const fragment = document.createDocumentFragment();
-const loader = document.getElementById("loader");
 
 window.onload = () => {
+  const loader = document.createElement("div");
+  const loaderImage = document.createElement("img");
+  loader.classList.add("loading");
+  loaderImage.src = "img/loading-circle.gif";
+  wrapper.appendChild(loader).appendChild(loaderImage);
+
   const fetchedData = new Promise((resolve, reject) => {
     const listItems = [
       { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
@@ -15,27 +21,27 @@ window.onload = () => {
     }, 2500);
   });
 
-function createNewList(data) {
-  data.forEach((key) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    const img = document.createElement("img");
+  function createNewList(data) {
+    data.forEach((key) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const img = document.createElement("img");
 
-    a.href = `/${key.to}`;
-    a.textContent = key.text;
-    img.src = key.img;
-    img.alt = key.alt;
+      a.href = `/${key.to}`;
+      a.textContent = key.text;
+      img.src = key.img;
+      img.alt = key.alt;
 
-    fragment
-      .appendChild(li)
-      .appendChild(a)
-      .insertAdjacentElement("afterbegin", img);
+      fragment
+        .appendChild(li)
+        .appendChild(a)
+        .insertAdjacentElement("afterbegin", img);
+    });
+    ul.appendChild(fragment);
+  }
+
+  fetchedData.then((listItems) => {
+    loader.classList.add("loaded");
+    createNewList(listItems);
   });
-  ul.appendChild(fragment);
-}
-
-fetchedData.then((listItems) => {
-  loader.classList.add('loaded');
-  createNewList(listItems);
-});
-}
+};
