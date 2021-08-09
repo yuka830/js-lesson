bad01286f0b6029ebf65693a32af81ea6e08a43e
yuka <66966699+yuka830@users.noreply.github.com>
2021-08-08 22:07:55 -0700

    Change: lesson11
    Rewrote part of fetching data.I use Fetch func this time so I don't need part of Promise and window.onload func.Made the code working more clearly moving fetched data into try part.

--- lesson11/main.js
+++ lesson11/main.js
@@ -15,61 +15,48 @@ const loaded = () => {
   loader.classList.add("loaded");
 };
 
-window.onload = () => {
-  loading();
+const jsonUrl = "https://jsondata.okiba.me/v1/json/PBH3M210806155341";
 
-  const fetchedData = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(listItems());
-    }, 2500);
+const createNewList = (data) => {
+  const ul = document.getElementById("js-lists");
+  const fragment = document.createDocumentFragment();
+
+  data.forEach((key) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = `/${key.a}`;
+    a.textContent = key.text;
+    img.src = key.img;
+    img.alt = key.alt;
+
+    fragment
+      .appendChild(li)
+      .appendChild(a)
+      .insertAdjacentElement("afterbegin", img);
   });
+  ul.appendChild(fragment);
+};
 
-  const listItems = async () => {
-    const jsonUrl = "https://jsondata.okiba.me/v1/json/PBH3M210806155341";
+const fetchedData = async () => {
+  try {
+    loading();
     const response = await fetch(jsonUrl);
     const json = await response.json();
     return json.data;
-  };
-
-  const createNewList = (data) => {
-    const ul = document.getElementById("js-lists");
-    const fragment = document.createDocumentFragment();
-
-    data.forEach((key) => {
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      const img = document.createElement("img");
-
-      a.href = `/${key.a}`;
-      a.textContent = key.text;
-      img.src = key.img;
-      img.alt = key.alt;
-
-      fragment
-        .appendChild(li)
-        .appendChild(a)
-        .insertAdjacentElement("afterbegin", img);
-    });
-    ul.appendChild(fragment);
-  };
-
-  const callLists = async () => {
-    try {
-      const listsContents = await fetchedData;
-      return listsContents;
-    } catch (error) {
-      const wrapper = document.getElementById("js-wrapper");
-      wrapper.textContent = "データの取得ができませんでした。";
-      console.error(error);
-    } finally {
-      loaded();
-    }
-  };
-
-  const init = async () => {
-    const val = await callLists();
-    createNewList(val);
-  };
-
-  init();
+  } catch (error) {
+    const wrapper = document.getElementById("js-wrapper");
+    wrapper.textContent = "データの取得ができませんでした。";
+    console.error(error);
+  } finally {
+    loaded();
+  }
 };
+
+const init = async () => {
+  const val = await fetchedData();
+  createNewList(val);
+};
+
+init();
