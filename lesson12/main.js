dbf1b68ae4f2118c6b3b8cc887059af74404ee67
yuka <66966699+yuka830@users.noreply.github.com>
2021-08-16 22:30:25 -0700

    Feat: lesson12
    copy the file from lesson11

---
+++ lesson12/main.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const createLoader = () => {
+  const wrapper = document.getElementById("js-wrapper");
+  const loader = document.createElement("div");
+  const loaderImage = document.createElement("img");
+  loader.id = "loader";
+  loader.classList.add("loading");
+  loaderImage.src = "img/loading-circle.gif";
+  wrapper.appendChild(loader).appendChild(loaderImage);
+};
+
+const loading = () => {
+  const loader = document.getElementById("loader");
+  loader.classList.add("loading");
+};
+
+const loaded = () => {
+  const loader = document.getElementById("loader");
+  loader.classList.add("loaded");
+};
+
+const jsonUrl = "https://jsondata.okiba.me/v1/json/PBH3M210806155341";
+
+const createNewList = (data) => {
+  const ul = document.getElementById("js-lists");
+  const fragment = document.createDocumentFragment();
+
+  data.forEach((key) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = `/${key.a}`;
+    a.textContent = key.text;
+    img.src = key.img;
+    img.alt = key.alt;
+
+    fragment
+      .appendChild(li)
+      .appendChild(a)
+      .insertAdjacentElement("afterbegin", img);
+  });
+  ul.appendChild(fragment);
+};
+
+const fetchedData = async () => {
+  try {
+    const response = await fetch(jsonUrl);
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    const wrapper = document.getElementById("js-wrapper");
+    wrapper.textContent = "データの取得ができませんでした。";
+    console.error(error);
+  } finally {
+    loaded();
+  }
+};
+
+const init = async () => {
+  createLoader();
+  loading();
+  const val = await fetchedData();
+  createNewList(val);
+};
+
+init();
