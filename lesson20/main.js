51f0f32a6adeac92c2d99a1e16931bb56c520921
yuka <66966699+yuka830@users.noreply.github.com>
2021-12-28 23:38:42 -0800

    Feat: lesson20
    Implemented user table

--- lesson20/main.js
+++ lesson20/main.js
@@ -1,1 +1,109 @@
 
+const jsonUrl = "https://myjson.dit.upm.es/api/bins/4qxd";
+const tableWrap = document.getElementById("js-table-wrapper");
+
+const createElementWithClassName = (element, name) => {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(name);
+  return createdElement;
+};
+
+const createLoader = () => {
+  const loader = document.createElement("div");
+  const loaderImage = document.createElement("img");
+  loader.id = "js-loader";
+  loaderImage.src = "img/loading-circle.gif";
+  loader.appendChild(loaderImage);
+  return loader;
+};
+
+const loading = () => {
+  const loader = document.getElementById("js-loader");
+  loader.classList.add("loading");
+};
+
+const loaded = () => {
+  const loader = document.getElementById("js-loader");
+  loader.classList.add("loaded");
+};
+
+const fetcheDataInSecond = (sec, jsonUrl) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(fetch(jsonUrl));
+    }, sec);
+  });
+};
+
+const fetchePersonalData = async () => {
+  try {
+    const response = await fetcheDataInSecond(3000, jsonUrl);
+    const json = await response.json();
+    return json.data;
+  } catch (e) {
+    tableWrap.textContent = "データの取得ができませんでした。";
+    console.error(e);
+  } finally {
+    loaded();
+  }
+};
+
+const renderTable = (usersData) => {
+  const table = createTable();
+  table.appendChild(createTableHeader(usersData));
+  table.appendChild(createTableData(usersData));
+  tableWrap.appendChild(table);
+};
+
+const createTable = () => {
+  const table = createElementWithClassName("table", "users-table");
+  table.id = "js-table";
+  return table;
+};
+
+const createTableHeader = (usersData) => {
+  const trOfThead = document.createElement("tr");
+  const fragment = document.createDocumentFragment();
+  for (let key in usersData[0]) {
+    const th = createElementWithClassName("th", "users-table__th");
+    th.textContent = key;
+    key === "id" && (th.textContent = key.toUpperCase());
+    fragment.appendChild(th);
+  }
+  trOfThead.appendChild(fragment);
+  return trOfThead;
+};
+
+const createTableData = (usersData) => {
+  const fragment = document.createDocumentFragment();
+  usersData.forEach((userData) => {
+    const trOfTdata = document.createElement("tr");
+    Object.keys(userData).forEach((key) => {
+      const td = createElementWithClassName("td", "users-table__td");
+      td.textContent = userData[key];
+      trOfTdata.appendChild(td);
+    });
+    fragment.appendChild(trOfTdata);
+  });
+  return fragment;
+};
+
+const init = async () => {
+  tableWrap.insertAdjacentElement("beforebegin", createLoader());
+  loading();
+  let usersData;
+  try {
+    usersData = await fetchePersonalData();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.log("処理が完了しました。");
+  }
+  if (usersData.length === 0) {
+    tableWrap.textContent = "データがありません。";
+    return;
+  } else {
+    renderTable(usersData);
+  }
+};
+init();
