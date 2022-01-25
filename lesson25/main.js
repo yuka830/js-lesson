303bee07e7dec4afb5e8416d840334ccff108015
yuka <yuka.m08@gmail.com>
2022-01-24 23:35:57 -0800

    Feat: lesson25
    Add the validation funton for the register form

--- lesson25/main.js
+++ lesson25/main.js
@@ -6,6 +6,19 @@ const openModal = document.getElementById("js-openModal");
 const closeModal = document.getElementById("js-closeModalBtn");
 const mask = document.getElementById("js-mask");
 
+const flags = {
+  userName: false,
+  email: false,
+  pass: false,
+  checkBox: false
+};
+
+const createElementWithClassName = (element, name) => {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(name);
+  return createdElement;
+};
+
 submitBtn.addEventListener(
   "click",
   (e) => {
@@ -19,13 +32,83 @@ submitBtn.addEventListener(
   false
 );
 
+const checkInputVal = (targetForm) => {
+  const targetVal = targetForm.value;
+  let result;
+
+  switch (targetForm.id) {
+    case "userName":
+      const minLength = 1;
+      const maxLength = 15;
+      result = targetVal.length >= minLength && targetVal.length <= maxLength;
+      break;
+    case "email":
+      const emailValid = /^\w[\w_.-]*@.+\..+/;
+      result = emailValid.test(targetVal);
+      break;
+    case "pass":
+      const passValid = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\w{8,}/;
+      result = passValid.test(targetVal);
+      break;
+    default:
+      console.error(`は見つかりませんでした`);
+  }
+
+  result ? (flags[targetForm.id] = true) : (flags[targetForm.id] = false);
+  return result;
+};
+
+const createErrorMessage = (targetFormId) => {
+  const errorTxt = createElementWithClassName("p", "error-txt");
+  errorTxt.id = "js-errorTxt";
+
+  switch (targetFormId) {
+    case "userName":
+      errorTxt.textContent = "ユーザー名は15文字以下にしてください。";
+      break;
+    case "email":
+      errorTxt.textContent = "メールアドレスの形式になっていません。";
+      break;
+    case "pass":
+      errorTxt.textContent =
+        "8文字以上の大小の英数字を交ぜたものにしてください。";
+      break;
+    default:
+      console.error(`は見つかりませんでした`);
+  }
+
+  return errorTxt;
+};
+
+const renderErrorMessage = (targetFormId) => {
+  const errorTxt = createErrorMessage(targetFormId);
+  const targetInput = document.getElementById(targetFormId);
+  targetInput.after(errorTxt);
+};
+
+//It's called in the HTML file
+const validateInputVal = (targetForm) => {
+  const check = checkInputVal(targetForm);
+  const errorTxtEle = targetForm.nextElementSibling;
+  !check && !errorTxtEle && renderErrorMessage(targetForm.id);
+  check && errorTxtEle && errorTxtEle.remove();
+  switchSbumitBtn();
+};
+
 const observer = new IntersectionObserver((targets) => {
   if (targets[0].intersectionRatio === 1) {
     checkBox.disabled = false;
     checkBox.checked = true;
+    flags.checkBox = true;
   }
+  switchSbumitBtn();
 });
 observer.observe(scrollEnd);
+
+const switchSbumitBtn = () => {
+  const checkFlags = Object.values(flags).every((val) => val);
+  checkFlags ? (submitBtn.disabled = false) : (submitBtn.disabled = true);
+};
 
 const showModal = () => {
   modal.classList.remove("hidden");
