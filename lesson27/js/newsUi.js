332187c0eab672031228a7c44c4d3c1f5d1bf13f
yuka <yuka.m08@gmail.com>
2022-02-19 18:50:49 -0800

    Lesson27 first commit

---
+++ lesson27/js/newsUi.js
@@ -0,0 +1,165 @@
+import { differenceInDays } from "date-fns";
+
+const jsonUrl = "https://myjson.dit.upm.es/api/bins/4iwh";
+const newsWrapper = document.getElementById("js-news");
+const tabUl = document.getElementById("js-news-tabs");
+
+const createElementWithClassName = (element, name) => {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(name);
+  return createdElement;
+};
+
+//comment-icon
+const renderCommentIcon = (element) => {
+  const commentSpan = createElementWithClassName("span", "comment-icon");
+  const commentImg = document.createElement("img");
+  commentImg.src = "./img/comment.png";
+  commentSpan.appendChild(commentImg);
+  element.appendChild(commentSpan);
+};
+
+//new-label
+const renderNewLabel = (element) => {
+  const newLabel = createElementWithClassName("span", "new-label");
+  newLabel.textContent = "NEW!";
+  element.appendChild(newLabel);
+};
+
+//check days between today and posted date for new-label
+const isWithinSpecificDays = (date) => {
+  const newLavelPeriod = 3;
+  const distanceDate = differenceInDays(new Date(), new Date(date));
+  return distanceDate <= newLavelPeriod;
+};
+
+//news-img
+const creatNewsImg = (element, category) => {
+  const imgWrapper = createElementWithClassName("div", "news__img");
+  const newsImg = document.createElement("img");
+  newsImg.src = `./img/${category}.png`;
+  imgWrapper.appendChild(newsImg);
+  element.appendChild(imgWrapper);
+};
+
+//tab
+const renderNewTab = (newsUiItems) => {
+  const tabFragment = document.createDocumentFragment();
+
+  newsUiItems.forEach((newsUiItem, index) => {
+    const tabLi = createElementWithClassName("li", "tab");
+    tabLi.textContent = newsUiItem.category;
+    tabLi.dataset.category = newsUiItem.category;
+    index === 0 && tabLi.classList.add("is-active");
+
+    tabLi.addEventListener("click", tabSwitch);
+    tabFragment.appendChild(tabLi);
+  });
+
+  tabUl.appendChild(tabFragment);
+};
+
+//news
+const createArticleTitles = ({ articles }) => {
+  const titleFragment = document.createDocumentFragment();
+  articles.forEach((article) => {
+    const contentLi = createElementWithClassName("li", "news__item");
+    const a = document.createElement("a");
+    a.textContent = article.title;
+    a.href = "#";
+    titleFragment.appendChild(contentLi).appendChild(a);
+
+    isWithinSpecificDays(article.date) && renderNewLabel(contentLi);
+    article.comments > 0 && renderCommentIcon(contentLi);
+  });
+  return titleFragment;
+};
+
+const createContentsItem = (newsUiItems) => {
+  const contentFragment = document.createDocumentFragment();
+  newsUiItems.forEach((newsUiItem, index) => {
+    const newsContent = createElementWithClassName("div", "news__content");
+    const elementForFlex = createElementWithClassName("div", "flex");
+    const contentUl = createElementWithClassName("ul", "js-news__lists");
+    const categoryName = newsUiItem.category;
+    newsContent.classList.add(`js-${categoryName}`);
+    //とりあえず最初のインデックスをアクティブなタブとする
+    index === 0 && newsContent.classList.add("is-show");
+    creatNewsImg(elementForFlex, categoryName);
+
+    contentFragment
+      .appendChild(newsContent)
+      .appendChild(elementForFlex)
+      .appendChild(contentUl)
+      .appendChild(createArticleTitles(newsUiItem));
+  });
+  return contentFragment;
+};
+
+//news
+const renderNewContent = (newsUiItems) =>
+  newsWrapper.appendChild(createContentsItem(newsUiItems));
+
+//tabSwitch
+const tabSwitch = () => {
+  tabUl.addEventListener("click", (e) => handleClick(e));
+};
+
+const handleClick = (e) => {
+  e.preventDefault();
+  initNewsElements();
+  showNewsElements(e);
+};
+
+const showNewsElements = (e) => {
+  const eventTarget = e.target;
+  const dataCategory = eventTarget.dataset.category;
+  eventTarget.classList.add("is-active");
+  const target = `.js-${dataCategory}`;
+  newsWrapper.querySelector(target).classList.add("is-show");
+};
+
+const initNewsElements = () => {
+  const activeTab = newsWrapper.querySelector(".is-active");
+  const activeContent = newsWrapper.querySelector(".is-show");
+  activeTab.classList.remove("is-active");
+  activeContent.classList.remove("is-show");
+};
+
+const fetchedNewsComponentData = async () => {
+  try {
+    const response = await fetch(jsonUrl);
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    newsWrapper.textContent = "データの取得ができませんでした。";
+    console.error(error);
+  } finally {
+    console.log("処理が終了しました。");
+  }
+};
+
+const createLogoutBtn = () => {
+  const btn = createElementWithClassName("button", "btn");
+  btn.id = "js-logout";
+  btn.textContent = "ログアウト";
+  return btn;
+};
+
+const renderLogoutBtn = () => {
+  const btn = createLogoutBtn();
+  btn.addEventListener("click", () => {
+    localStorage.removeItem("token");
+    window.location.href = "./login.html";
+  });
+  newsWrapper.after(btn);
+};
+
+const init = async () => {
+  const newsUiItems = await fetchedNewsComponentData();
+  renderNewTab(newsUiItems);
+  renderNewContent(newsUiItems);
+  renderLogoutBtn();
+};
+
+init();
---
+++ lesson27/js/register.js
@@ -0,0 +1,129 @@
+const checkBox = document.getElementById("js-checkBox");
+const submitBtn = document.getElementById("js-submitBtn");
+const scrollEnd = document.getElementById("js-scrollEnd");
+const modal = document.getElementById("js-modal");
+const openModal = document.getElementById("js-openModal");
+const closeModal = document.getElementById("js-closeModalBtn");
+const mask = document.getElementById("js-mask");
+
+const userNameInput = document.getElementById("userName");
+const mailInput = document.getElementById("email");
+const passInput = document.getElementById("pass");
+
+const flags = {
+  userName: false,
+  email: false,
+  pass: false,
+  checkBox: false
+};
+
+const createElementWithClassName = (element, name) => {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(name);
+  return createdElement;
+};
+
+submitBtn.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    if (!checkBox.checked) {
+      alert("利用規約を全て確認後、チェックボタンを押してから送信して下さい。");
+      return;
+    }
+    window.location.href = "./register-done.html";
+  },
+  false
+);
+
+checkBox.addEventListener(
+  "click",
+  (e) => {
+    flags.checkBox = e.target.checked;
+    switchSubmitBtn();
+  },
+  false
+);
+
+const validations = {
+  userName: {
+    maxLength: 15,
+    minLength: 1,
+    errorMessage: "※ユーザー名は1文字以上15文字以下にしてください。",
+    isValid: (val) => {
+      return (
+        val.length >= validations.userName.minLength &&
+        val.length <= validations.userName.maxLength
+      );
+    }
+  },
+  email: {
+    errorMessage: "メールアドレスが空欄・もしくは形式が異なっています。",
+    isValid: (val) => /.+@.+\..+/.test(val)
+  },
+  pass: {
+    errorMessage: "8文字以上の大小の英数字を交ぜたものにしてください。",
+    isValid: (val) =>
+      /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,}$/.test(val)
+  }
+};
+
+const checkInputVal = (targetForm) => {
+  const targetInput = document.getElementById(targetForm.id);
+  const result = validations[targetForm.id].isValid(targetForm.value);
+  if (result) {
+    flags[targetForm.id] = true;
+    return;
+  }
+  flags[targetForm.id] = false;
+  targetInput.after(createErrorMessage(targetForm));
+};
+
+const createErrorMessage = (targetForm) => {
+  const errorTxt = createElementWithClassName("p", "error-txt");
+  errorTxt.textContent = validations[targetForm.id].errorMessage;
+  return errorTxt;
+};
+
+const validateInputVal = (targetForm) => {
+  const errorTxtEle = targetForm.nextElementSibling;
+  checkInputVal(targetForm);
+  errorTxtEle && errorTxtEle.remove();
+  switchSubmitBtn();
+};
+
+userNameInput.addEventListener("blur", (e) => validateInputVal(e.target));
+mailInput.addEventListener("blur", (e) => validateInputVal(e.target));
+passInput.addEventListener("blur", (e) => validateInputVal(e.target));
+
+const observer = new IntersectionObserver((targets) => {
+  if (targets[0].intersectionRatio === 1) {
+    checkBox.disabled = false;
+    checkBox.checked = true;
+    flags.checkBox = true;
+  }
+  switchSubmitBtn();
+});
+observer.observe(scrollEnd);
+
+const switchSubmitBtn = () => {
+  const checkFlags = Object.values(flags).every((val) => val);
+  checkFlags ? (submitBtn.disabled = false) : (submitBtn.disabled = true);
+};
+
+const showModal = () => {
+  modal.classList.remove("hidden");
+  mask.classList.remove("hidden");
+};
+
+const hiddenModal = () => {
+  modal.classList.add("hidden");
+  mask.classList.add("hidden");
+};
+
+mask.addEventListener("click", () => {
+  closeModal.click();
+});
+
+openModal.addEventListener("click", showModal);
+closeModal.addEventListener("click", hiddenModal);
